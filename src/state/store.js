37c5689af070d4caaa531337db1c303295987da7
Md Nuruzzaman Chowdhury <devnuruzzaman@gmail.com>
2021-08-04 12:54:31 +0600

    React with redux initial commit just learning purpose

---
+++ src/state/store.js
@@ -0,0 +1,11 @@
+import { applyMiddleware, createStore, compose } from 'redux';
+import reducers from './reducers/index';
+import thunk from 'redux-thunk';
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+export const store = createStore(
+  reducers,
+  {},
+  composeEnhancers(applyMiddleware(thunk))
+);
